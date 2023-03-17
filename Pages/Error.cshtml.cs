f259519f4040cd1e7664884ecb539ab9ac8eb099
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2023-03-17 23:04:18 +0500

    form response

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -25,3 +25,26 @@ public class ErrorModel : PageModel
     }
 }
 
+namespace YourNamespace.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Return the HTML form to the user
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(string name, string email)
+        {
+            // Here, you can write your code to handle the form submission.
+            // You can access the form data through the 'name' and 'email' parameters.
+            // For example, you could save the data to a database or send an email.
+
+            // After handling the form submission, you can return a response to the user.
+            return Content("Thanks for submitting the form!");
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -20,3 +20,39 @@ public class IndexModel : PageModel
     {        
     }
 }
+namespace YourNamespace
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
